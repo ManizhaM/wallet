c283c2070ea2a0d8a39f24622f535e08b65abb8a
Leo <charmed_devil@list.ru>
2021-11-09 13:31:03 +0500

    добавлены авто-тесты к методу FindPaymentByID

--- pkg/wallet/service_test.go
+++ pkg/wallet/service_test.go
@@ -61,3 +61,34 @@ func TestService_Reject_notFound(t *testing.T) {
 		t.Errorf("invalid result, expected: %v, actual: %v", expected, err)
 	}
 }
+
+
+func TestService_FindPaymentByID_success(t *testing.T) {
+	s := &Service{}
+	acc, err := s.RegisterAccount("+992000000001")
+	if err != nil {
+		t.Error(err)
+	}
+	err1 := s.Deposit(acc.ID, 2000)
+	if err1 != nil {
+		t.Error(err1)
+	}
+	pay, err2 := s.Pay(1, 1000, "auto")
+	if err2 != nil {
+		t.Error(err2)
+	}
+	_, err3 := s.FindPaymentByID(pay.ID)
+
+	if !reflect.DeepEqual(nil, err3) {
+		t.Errorf("invalid result, expected: %v, actual: %v", nil, err3)
+	}
+}
+
+func TestService_FindPaymentByID_notFound(t *testing.T) {
+	s := &Service{}
+	_, err := s.FindPaymentByID("1111")
+	expected := ErrPaymentNotFound
+	if !reflect.DeepEqual(expected, err) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, err)
+	}
+}
